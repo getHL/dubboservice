f368867b48f6f0de942a975c205acb92435adb02
unknown <liangzhijian@hikvision.com>
2016-07-22 12:07:45 +0800

    dubbo + spring service

---
+++ src/main/java/service/impl/WelcomeServiceImpl.java
@@ -0,0 +1,12 @@
+package service.impl;
+
+import service.WelcomeService;
+
+public class WelcomeServiceImpl implements WelcomeService{
+
+	public String sayHello() {
+		// TODO Auto-generated method stub
+		return "Hello consumers !!!";
+	}
+
+}
